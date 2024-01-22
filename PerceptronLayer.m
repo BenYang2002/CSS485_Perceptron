6f670c37cd5cb33449a349540859054c20816ee1
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-01-22 15:24:15 -0800

    Update PerceptronLayer.m
    question 2 update(now the perceptron can learn yay)

--- PerceptronLayer.m
+++ PerceptronLayer.m
@@ -2,8 +2,23 @@ classdef PerceptronLayer
     properties
         weight_matrix
         bias
+
+        %name of the activation function
         transfer
+        
+        % the whole input set
+        input_matrix
+
+        %the current single input
+        input_vector
+
+        %the whole standard/correct output
+        output_matrix
+        
+        %the current standard/correct output
+        output_vector
     end
+
     % this is the constructor where input 1 is the weight,
     % input 2 is the bias and the third argument is the activation function name
     methods
@@ -25,20 +40,106 @@ classdef PerceptronLayer
             obj.transfer = funcName;
         end
 
+        % input: takes one parameter
+        % input: the input matrix
+        % it serves as the setter for input matrix
+        function obj = input_setter(obj,input)
+            % this assumes each column is a set of input
+            if size(obj.weight_matrix,2) ~= size(input,1)
+                error("Perceptron.input: dimention of the training set" + ...
+                    " does not match with weight matrix");
+                return;
+            end
+            obj.input_matrix = input;
+        end
+
+        % output: takes one parameter
+        % output: the output matrix
+        % it serves as the setter for output matrix
+        function obj = output_setter(obj,output)
+            % this assumes each column is a set of input
+            if size(obj.weight_matrix,1) ~= size(output,1)
+                error("Perceptron.input: dimention of the output" + ...
+                    " does not match with weight matrix");
+                return;
+            end
+            obj.output_matrix = output;
+        end
+
+        % errorLoss takes in two parameters:
+        % a: the prediction
+        % t: the expected/correct output
+        % return the vector of t - a
         function output = errorLoss(obj,a,t)
             if size(a,2) ~= 1 || size(t,2) ~= 1
-                disp("err:expect a vector");
+                erro("perceptron.errorLoss: expect a vector");
                 return;
             elseif size(a,1) ~= size(t,1)
-                disp("err: dimension of a and t does not match ");
+                disp("perceptron.errorLoss:  dimension of a and t does not" + ...
+                    " match ");
                 return;
             end
             output = t - a;
         end
 
-% this function let the perceptron learn about the weights and set the
-% highest iteration to 1000
-function output = learn(obj,input_set,output_set)
+        % backward takes in one parameters:
+        % error: the vector of errors
+        % this function update the bias and weight matrix
+        function obj = backward(obj,error)
+            % this assumes error is a vector
+            % if the dimension does not match with neuron number, display
+            % error msg
+            if size(error,1) ~= size(obj.weight_matrix,1)
+                error("perceptron.backward: dimension of error does not" + ...
+                    " match with neuron number");
+                return;
+            end
+
+            % update bias
+            obj.bias = obj.bias + error;
+
+            % update weight matrix
+            fix_matrix = [];
+            for i = 1:length(error)
+                fix_matrix = [fix_matrix ; (obj.input_vector * error(i))' ];
+            end
+            obj.weight_matrix = obj.weight_matrix + fix_matrix;
+        end
+        
+        % print does not take any parameter
+        % this function prints out the weight matrix and bias
+        function print(obj)
+            disp("weight_matrix: ");
+            disp(obj.weight_matrix);
+            disp("bias: ");
+            disp(obj.bias);
+        end
+
+        function obj = learn2(obj);
+            correct = false ;
+            count = 1;
+            while (~correct & count <= 1000)
+                correct = true;
+                for i = 1:size(obj.input_matrix,2)
+                    obj.input_vector = obj.input_matrix(:,i);
+                    obj.output_vector = obj.output_matrix(:,i);
+                    prediction = obj.forward1(obj.input_vector);
+                    error = obj.errorLoss(prediction,obj.output_vector);
+                    if ~all(error == 0)
+                        correct = false;
+                    end
+                    obj = obj.backward(error);
+                    count = count + 1;
+                end
+            end
+            if correct
+                obj.print();
+            end
+        end
+
+        % this function let the perceptron learn about the weights and set the
+        % highest iteration to 1000
+        function output = learn(obj,input_set,output_set)
             %each row of the training set is a single set
             %each column of the output set is the correct output for each
             %set of input
@@ -75,9 +176,10 @@ function output = learn(obj,input_set,output_set)
                 count = count + 1;
             end
         end
-% forward using matrix * vector
+        
+        % forward using matrix * vector
         function output = forward1(obj,input)
-            netinput = obj.weight_matrix * input' + obj.bias;
+            netinput = obj.weight_matrix * input + obj.bias;
             if strcmp(obj.transfer, "hardlim")
                 output = hardlim(netinput);
             else
@@ -85,7 +187,7 @@ function output = learn(obj,input_set,output_set)
             end
         end
 
- %forward through each neuron(iterate through each neuron)
+        %forward through each neuron(iterate through each neuron)
         function outputVec = forward2(obj,input)
             outputVec = [];
             inVec = input';
@@ -105,22 +207,22 @@ function output = learn(obj,input_set,output_set)
         end
 
     end
-    methods (Access = private) % set private
-        %hardlim transformation func
-        function result = hardlim(netInput)
-            if netInput < 0
-                result = 0;
-            else
-                result = 1;
+        methods (Access = private) % set private
+            %hardlim transformation func
+            function result = hardlim(netInput)
+                if netInput < 0
+                    result = 0;
+                else
+                    result = 1;
+                end
             end
-        end
-        %hardlims transformation func
-        function result = hardlims(netInput)
-            if netInput < 0
-                result = -1;
-            else
-                result = 1;
+            %hardlims transformation func
+            function result = hardlims(netInput)
+                if netInput < 0
+                    result = -1;
+                else
+                    result = 1;
+                end
             end
         end
     end
-end
