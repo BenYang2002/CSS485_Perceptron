e7b7cd59703a9fa5e343afdb938219da99a1e810
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-01-28 04:03:52 -0800

    Update PerceptronLayer.m
    only one more clean up is needed before submission

--- PerceptronLayer.m
+++ PerceptronLayer.m
@@ -2,23 +2,19 @@ classdef PerceptronLayer
     properties
         weight_matrix
         bias
-
         %name of the activation function
         transfer
         
         % the whole input set
         input_matrix
-
         %the current single input
         input_vector
-
         %the whole standard/correct output
         output_matrix
         
         %the current standard/correct output
         output_vector
     end
-
     % this is the constructor where input 1 is the weight,
     % input 2 is the bias and the third argument is the activation function name
     methods
@@ -39,7 +35,6 @@ classdef PerceptronLayer
             end
             obj.transfer = funcName;
         end
-
         % input: takes one parameter
         % input: the input matrix
         % it serves as the setter for input matrix
@@ -52,7 +47,6 @@ classdef PerceptronLayer
             end
             obj.input_matrix = input;
         end
-
         % output: takes one parameter
         % output: the output matrix
         % it serves as the setter for output matrix
@@ -65,7 +59,6 @@ classdef PerceptronLayer
             end
             obj.output_matrix = output;
         end
-
         % errorLoss takes in two parameters:
         % a: the prediction
         % t: the expected/correct output
@@ -81,7 +74,6 @@ classdef PerceptronLayer
             end
             output = t - a;
         end
-
         % backward takes in one parameters:
         % error: the vector of errors
         % this function update the bias and weight matrix
@@ -94,15 +86,18 @@ classdef PerceptronLayer
                     " match with neuron number");
                 return;
             end
-
             % update bias
             obj.bias = obj.bias + error;
 
             % update weight matrix
+            fix_matrix = [];
+            for i = 1:length(error)
+                fix_matrix = [fix_matrix ; (obj.input_vector * error(i))' ];
+            end
             fix_matrix = (obj.input_vector * error')';
             obj.weight_matrix = obj.weight_matrix + fix_matrix;
         end
-        
+
         % print does not take any parameter
         % this function prints out the weight matrix and bias
         function print(obj)
@@ -111,8 +106,7 @@ classdef PerceptronLayer
             disp("bias: ");
             disp(obj.bias);
         end
-
-        function obj = learn2(obj);
+        function obj = learn(obj);
             correct = false ;
             count = 1;
             while (~correct & count <= 1000)
@@ -120,7 +114,7 @@ classdef PerceptronLayer
                 for i = 1:size(obj.input_matrix,2)
                     obj.input_vector = obj.input_matrix(:,i);
                     obj.output_vector = obj.output_matrix(:,i);
-                    prediction = obj.forward1(obj.input_vector);
+                    prediction = obj.forward(obj.input_vector);
                     error = obj.errorLoss(prediction,obj.output_vector);
                     if ~all(error == 0)
                         correct = false;
@@ -133,77 +127,16 @@ classdef PerceptronLayer
                 obj.print();
             end
         end
-
-        % this function let the perceptron learn about the weights and set the
-        % highest iteration to 1000
-        function output = learn(obj,input_set,output_set)
-            %each row of the training set is a single set
-            %each column of the output set is the correct output for each
-            %set of input
-            x = [obj.weight_matrix, obj.bias];
-            %now z is the input set with 1 vector at the last column
-            z = [input_set,ones(size(input_set,1))];
-            z = z'; % each column of z is a input vector
-            % a row of x times a column of z: ix * zi = the netinput for ith neuron
-            error = 0;
-            condition = false ;
-            count = 1;
-            while (~condition & count <= 1000)
-                condition = true;
-                for i = 1:size(input_set,1)
-                    prediction = obj.forward1(input_set(i,:));
-                    error = output_set(:,i) - prediction;
-                    fix_matrix = [];
-                    for j = 1:length(error)
-                        fix_matrix = [fix_matrix ; input_set(i,:) * error(j) ];
-                    end
-                    obj.weight_matrix = obj.weight_matrix + fix_matrix;
-                    obj.bias = obj.bias + error;
-                    disp("count: " + count);
-                    disp("bias: ");
-                    disp(obj.bias);
-                    disp("weight_matrix: ");
-                    disp(obj.weight_matrix);
-                    disp("error: ");
-                    disp(error);
-                    if ~all(error == 0)
-                        condition = false;
-                    end
-                end
-                count = count + 1;
-            end
-        end
-        
         % forward using matrix * vector
         % forward can also take a matrix input
-        function output = forward1(obj,input)
-            netinput = obj.weight_matrix * input' + obj.bias;
+        function output = forward(obj,input)
+            netinput = obj.weight_matrix * input + obj.bias;
             if strcmp(obj.transfer, "hardlim")
-                output = obj.hardlim(netinput);
+                output = hardlim(netinput);
             else
-                output = obj.hardlims(netinput);
+                output = hardlims(netinput);
             end
         end
-
-        %forward through each neuron(iterate through each neuron)
-        function outputVec = forward2(obj,input)
-            outputVec = [];
-            inVec = input';
-            biasVec = obj.bias';
-            for i = 1:size(obj.weight_matrix,1)
-                ithNeuron = 0;
-                ithNeuron = obj.weight_matrix(i,:) * inVec + biasVec(i);
-                output = 0;
-                if strcmp(obj.transfer, "hardlim")
-                    output = obj.hardlim(ithNeuron);
-                else
-                    output = obj.hardlims(ithNeuron);
-                end
-                outputVec = [outputVec,output];
-            end
-            outputVec = outputVec';
-        end
-
     end
         methods (Access = private) % set private
             %hardlim transformation func
